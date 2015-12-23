20b2d7132e70799e339289c7e54930e50a1494d1
swh <swhbox@foxmail.com>
2015-12-23 15:00:33 +0800

    update

--- lib/tplex/example/main.go
+++ lib/tplex/example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/coscms/xweb/lib/tplex"
 	"github.com/coscms/xweb/log"
@@ -10,8 +11,13 @@ import (
 
 func main() {
 	tpl := tplex.New(log.New(os.Stdout, "", log.Ldefault()), "./template/", true)
-	str := tpl.Fetch("test.html", nil, map[string]string{
-		"test": "one",
-	})
-	fmt.Printf("%v", str)
+	for i := 0; i < 5; i++ {
+		ts := time.Now()
+		fmt.Printf("==========%v: %v========\\\n", i, ts)
+		str := tpl.Fetch("test.html", nil, map[string]string{
+			"test": "one->" + fmt.Sprintf("%v", i),
+		})
+		fmt.Printf("%v\n", str)
+		fmt.Printf("==========cost: %v========/\n", time.Now().Sub(ts).Seconds())
+	}
 }
